ae3597df8bb57ff687fa469acc22c03a76b17286
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 09:24:28 -0400

    const PI
    8 decimal places

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
 
 let diameter = 1;
-let pi = 3.14;
+const PI = 3.14159265;
 let circumference = pi * diameter;
 
 console.log(`circumference is ${circumference}`);
