d9e72288ebb5700da07d268a80469b89047829ba
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 08:53:30 -0400

    updated pi calc
    changed console and pi variable.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
 
 let diameter = 1;
-let pi = 3.14;
+const pi = 3.14152;
 let circumference = pi * diameter;
 
-console.log(`circumference is ${circumference}`);
+console.log(`Circumference is ${circumference}`);
