408cc3383a4e74c83e05242c332a7e78a09109ca
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 09:02:08 -0400

    updated main.js
    made pi more accurate and a const

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
 
 let diameter = 1;
-let pi = 3.14;
+const pi = 3.1415;
 let circumference = pi * diameter;
 
 console.log(`Circumference is ${circumference}`);
