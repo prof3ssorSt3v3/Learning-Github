ea827e6bab2befa938f1579cb703c54f6373050b
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 09:04:06 -0400

    updated pi to be more accurate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
 
 let diameter = 1;
-let pi = 3.14;
+const PI = 3.141592;
 let circumference = pi * diameter;
 
 console.log(`Circumference is ${circumference}`);
