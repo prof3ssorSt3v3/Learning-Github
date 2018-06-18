73845323886e93de0181dac3eee029d195f0e9e8
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 08:58:22 -0400

    revert main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
 
 let diameter = 1;
-const pi = 3.14152;
+let pi = 3.14;
 let circumference = pi * diameter;
 
 console.log(`Circumference is ${circumference}`);
