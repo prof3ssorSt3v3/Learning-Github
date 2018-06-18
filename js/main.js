346033d19bfae1237e5111880aa1b6e5d6eca9b3
Steve Griffith <prof3ssorSt3v3@outlook.com>
2018-06-18 08:44:11 -0400

    Revert "Added circumference calculation"
    This reverts commit d9d8503547288a22e4679efffc3b3796d77fad1b.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
 let colour = '#'+((1<<24)*Math.random()|0).toString(16).padStart(6,0);
-
-let diameter = 1;
-let pi = 3.14;
-let circumference = pi * diameter;
-
-console.log(`circumference is ${circumference}`);
